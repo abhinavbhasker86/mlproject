de71acd9b9afba6dafc88ffd06f9173cdcea5295
abhinavbhasker86 <abbhaske@gmail.com>
2025-07-05 14:06:40 -0400

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,17 +1,24 @@
 import sys
 import logging
+
+#Function to generate a detailed error message including file name and line number
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message=" Error Occured in python script name[{0}] line Number[{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str(error))
+    _,_,exc_tb=error_detail.exc_info() # Get traceback object
+    file_name = exc_tb.tb_frame.f_code.co_filename # Get the name of the file where the error occurred
+
+    # Construct the error message with file name, line number, and error message
+    error_message=" Error Occured in python script name: [{0}], line Number: [{1}], error message: [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
     return error_message
 
+# Custom exception class to handle exceptions with detailed error messages
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
+    def __init__(self,error_message,error_detail:sys):  
+        super().__init__(error_message) # Call the base class constructor with the error message
+        # Store the detailed error message
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
+        # Return the detailed error message when the exception is printed
         return self.error_message
 
 
--- src/logger.py
+++ src/logger.py
@@ -2,12 +2,17 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# Generate a log file name with the current date and time
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Create the logs directory path and ensure it exists
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
+# Define the full path for the log file
 LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 
+# Configure the logging module to write logs to the specified file
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s -%(message)s",
